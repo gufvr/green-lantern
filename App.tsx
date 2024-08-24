a45344fb1c7553cac32faeccab960011f141dcf0
GuFvr <gustavo.vfavero@gmail.com>
2024-08-24 20:10:42 -0300

    feat: finalize project with toggle functionality for symbol display

--- App.tsx
+++ App.tsx
@@ -1,19 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import symbolOn from './assets/pics/symbol-on.png';
+import symbolOff from './assets/pics/symbol-off.png';
+import React, { useState } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+  const [isActive, setIsActive] = useState(false);
 
+  function handleSymbol() {
+    setIsActive((isActive) => {
+      return !isActive;
+    });
+  }
+
+  return (
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={handleSymbol}>
+        <Image source={isActive ? symbolOn : symbolOff} />
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerOff: {
+    flex: 1,
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
